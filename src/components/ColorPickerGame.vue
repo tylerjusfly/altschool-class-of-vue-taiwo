66206b3ab7e807cdc10ff6a8370c8a43d5a48ce6
tylerjusfly <tylerjusfly1@gmail.com>
2023-02-07 15:57:18 +0100

    extracting colorpicker composables

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -1,36 +1,18 @@
 <template>
-  <h1>Color Picker Game</h1>
+    <h1>Color Picker Game</h1>
 
-  <div>{{ message }}</div>
+    <div>{{ message }}</div>
 
-  <button v-for="color in colors" :key="color" @click="matchColor(color)">
-    {{ color }}
-  </button>
+    <button v-for="color in colors" :key="color" @click="matchColor(color)">
+        {{ color }}
+    </button>
 </template>
 
-<script>
+<script setup>
 import { ref, reactive } from "@vue/reactivity";
+import { UseColorPicker } from "../composables/UseColorPicker";
 
-export default {
-  setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
-
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
-
-    return { colors, message, matchColor };
-  },
-};
+const { colors, message, matchColor } = UseColorPicker();
 </script>
 
 <style></style>
---
+++ src/composables/UseColorPicker.js
@@ -0,0 +1,20 @@
+import { ref } from "vue";
+
+export const UseColorPicker = () => {
+    const colors = ref(["green", "red", "blue", "purple"]);
+    let message = ref("Pick a color...");
+
+    const matchColor = (value) => {
+        // do a random color based on the array index;
+        const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+
+        if (colors.value[randomNumber] === value) {
+            message.value = `You win... [answer: ${colors.value[randomNumber]}]`;
+            return;
+        }
+
+        message.value = `You loose [answer: ${colors.value[randomNumber]}]`;
+    };
+
+    return { colors, message, matchColor };
+};
